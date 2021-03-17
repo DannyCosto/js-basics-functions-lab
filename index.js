ded0a040f2917ad8f528d74019ab4179afb42046
Danny <danny.costelloe@gmail.com>
2021-03-16 20:18:40 -0400

    commit tree 45ac9b92f2604608a0666dc8f60c7a13a4bbda10

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const hq = 42
+const feet = 264
+
+function distanceFromHqInBlocks(dropOff){
+    return Math.abs(hq - dropOff);
+}
+
+function distanceFromHqInFeet(dropOff) {
+    return distanceFromHqInBlocks(dropOff) * feet;
+  }
+
+function distanceTravelledInFeet(pickUp, dropOff) {
+    return Math.abs(pickUp - dropOff)* feet;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance === feet) {
+      return 0;
+    }
+    else if (400 <= distance && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    }
+    else if (2000 <= distance && distance <= 2500) {
+      return 25;
+    }
+    else if (2500 < distance) {
+      return 'cannot travel that far';
+    }
+  }
